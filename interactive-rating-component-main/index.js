e3a876a123b14cd3c365da97684d77feef62a221
Gabriel <gabrielmd123@gmail.com>
2024-01-07 17:47:00 -0300

    interactive-rating: add loading state

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -7,24 +7,58 @@ const ratingCard = document.querySelector(".card.rating")
 /** @type {HTMLDivElement} */
 const resultCard = document.querySelector(".card.result")
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
 
     const rating = formData.get("rating")
 
-    handleRatingSubmit(rating)
+    await handleRatingSubmit(rating)
 })
 
 /**
  * @param {string} rating
  */
-function handleRatingSubmit(rating) {
-    ratingCard.classList.add("hidden")
-    resultCard.classList.remove("hidden")
+async function handleRatingSubmit(rating) {
+    try {
+        toggleLoadingStatus(true)
+        await new Promise(res => setTimeout(res, 3000))
 
-    /** @type {HTMLSpanElement} */
-    const resultPlacement = document.querySelector("[data-result]")
+        ratingCard.classList.add("hidden")
+        resultCard.classList.remove("hidden")
 
-    resultPlacement.innerText = rating
+        /** @type {HTMLSpanElement} */
+        const resultPlacement = document.querySelector("[data-result]")
+
+        resultPlacement.innerText = rating
+    } catch (error) {
+        console.log(error)
+    } finally {
+        toggleLoadingStatus(false)
+
+    }
+}
+
+/**
+ * @param {boolean} bool
+ */
+function toggleLoadingStatus(bool) {
+
+
+    /** @type {NodeListOf<HTMLInputElement>}*/
+    const inputs = document.querySelectorAll("[type='radio']")
+
+    /** @type {HTMLButtonElement}*/
+    const submitButton = document.querySelector('button')
+
+    inputs.forEach(input => {
+        bool
+            ? input.setAttribute("disabled", "")
+            : input.removeAttribute("disabled")
+    })
+
+    bool
+        ? submitButton.setAttribute("disabled", "")
+        : submitButton.removeAttribute("disabled")
+
 }

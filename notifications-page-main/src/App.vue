2c6b87130be16d3f2ac374c4fc40227edd13aff8
Gabriel <gabrielmd123@gmail.com>
2024-01-27 08:40:52 -0300

    add vue setup

---
+++ notifications-page-main/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <p>Notifications 3</p>
+
+    <p>Mark all as read</p>
+
+    <p>Mark Webber reacted to your recent post My first tournament today!</p>
+    <p>1m ago</p>
+
+    <p>Angela Gray followed you</p>
+    <p>5m ago</p>
+
+    <p>Jacob Thompson has joined your group Chess Club</p>
+    <p>1 day ago</p>
+
+    <p>Rizky Hasanuddin sent you a private message</p>
+    <p>5 days ago</p>
+    <p>
+      Hello, thanks for setting up the Chess Club. I've been a member for a few
+      weeks now and
+    </p>
+    <p>I'm already having lots of fun and improving my game.</p>
+
+    <p>Kimberly Smith commented on your picture</p>
+    <p>1 week ago</p>
+
+    <p>
+      Nathan Peterson reacted to your recent post 5 end-game strategies to
+      increase your win rate
+    </p>
+    <p>2 weeks ago</p>
+
+    <p>Anna Kim left the group Chess Club</p>
+    <p>2 weeks ago</p>
+  </main>
+</template>
+
+<style lang="scss"></style>
